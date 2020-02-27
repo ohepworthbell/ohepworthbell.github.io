b11b9729c876d9dc8eb4906e9215e5a231d79f71
Oliver Hepworth-Bell <oliver.hepworth@googlemail.com>
2020-02-27 22:23:33 +0000

    Added favicons

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin');
 
 module.exports = {
   entry: ['./src/scss/style.scss'],
@@ -12,10 +14,33 @@ module.exports = {
     new webpack.ProgressPlugin(),
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/favicon/logo.svg'),
+      inject: true,
+      outputPath: './',
+      prefix: '/',
+      favicons: {
+        appName: 'Portfolio',
+        appDescription: 'Portfolio of Oliver Hepworth-Bell',
+        background: '#121822',
+        theme_color: '#fff',
+        prefix: '/',
+        start_url: '/',
+        inject: true,
+        cache: true,
+        icons: {
+          coast: false,
+          yandex: false,
+          firefox: false,
+          appleStartup: false
+        }
+      }
+    }),
     new HtmlWebpackPlugin({
       template: 'src/html/index.html',
-      inject: false
+      excludeAssets: [/main.js/]
     }),
+    new HtmlWebpackExcludeAssetsPlugin(),
     new CopyPlugin([
       {
         from: 'src/images/',
